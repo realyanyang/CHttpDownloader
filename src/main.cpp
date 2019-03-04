841dc54b55430a45ead00bb737112eb32eaab5db
YanYangB <45633038+YanYangB@users.noreply.github.com>
2019-03-04 22:43:44 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,32 @@
 #include "Downloader.h"
 #include <thread>
 
+
+//bool downloadSuccess[threadNum] = { 0 };
+
 int main()
 {
 
-	HttpDownloader downloader("https://files.pythonhosted.org/packages/36/fa/51ca4d57392e2f69397cd6e5af23da2a8d37884a605f9e3f2d3bfdc48397/pip-19.0.3.tar.gz","D:\\Download\\testDownloader.gz");
+	HttpDownloader downloader("http://selenium-release.storage.googleapis.com/2.50/selenium-java-2.50.0.zip","D:\\Download\\testDownloader.gz");
 	//downloader.startDownloader();
-	downloader.singleDown();
+	//downloader.singleDown();
 	//downloader.newDownladThread(0, 276, 0);
+	start_end *tmp;
+	tmp = downloader.getStartEnd();
+	for (int i = 0; i < threadNum; i++)
+	{
+		/*cout << i << "start::" << (tmp + i)->startPoint << endl;
+		cout << i << "end::" << (tmp + i)->endPonint << endl;*/
+		thread anThread(&HttpDownloader::newDownladThread, downloader, (tmp + i)->startPoint, (tmp + i)->endPonint, i);
+		anThread.detach();
+	}
+	while (true)
+	{
+		if (downloader.threadMonitor())
+			break;
+		else
+			continue;
+	}
+		
 	return 0;
 }
