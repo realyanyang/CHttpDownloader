56588b7d163945268917a350a7de24ff60d0c0c4
YanYangB <45633038+YanYangB@users.noreply.github.com>
2019-03-05 21:44:54 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -11,27 +11,35 @@
 
 int main()
 {
+	clock_t starttime = clock();
 
-	HttpDownloader downloader("http://selenium-release.storage.googleapis.com/2.50/selenium-java-2.50.0.zip","D:\\Download\\testDownloader.gz");
-	//downloader.startDownloader();
-	//downloader.singleDown();
-	//downloader.newDownladThread(0, 276, 0);
-	start_end *tmp;
-	tmp = downloader.getStartEnd();
-	for (int i = 0; i < threadNum; i++)
+	HttpDownloader downloader("http://mirrors.163.com/debian/ls-lR.gz","D:\\Download\\testDownloader.gz");
+	if (downloader.getResumable())   //分段下载
 	{
-		/*cout << i << "start::" << (tmp + i)->startPoint << endl;
-		cout << i << "end::" << (tmp + i)->endPonint << endl;*/
-		thread anThread(&HttpDownloader::newDownladThread, downloader, (tmp + i)->startPoint, (tmp + i)->endPonint, i);
-		anThread.detach();
+		start_end *tmp;
+		tmp = downloader.getStartEnd();
+		for (int i = 0; i < threadNum; i++)
+		{
+			/*cout << i << "start::" << (tmp + i)->startPoint << endl;
+			cout << i << "end::" << (tmp + i)->endPonint << endl;*/
+			string tmpfile = downloader.creatTmpFile((tmp + i)->startPoint, (tmp + i)->endPonint, i);
+			thread anThread(&HttpDownloader::newDownladThread, downloader, (tmp + i)->startPoint, (tmp + i)->endPonint, tmpfile, i);
+			anThread.detach();
+		}
+		while (true)
+		{
+			if (downloader.threadMonitor())
+				break;
+			else
+				continue;
+		}
+		downloader.mergeTempFile();
 	}
-	while (true)
-	{
-		if (downloader.threadMonitor())
-			break;
-		else
-			continue;
-	}
-		
+	else
+		downloader.singleDown();
+
+	clock_t endtime = clock();
+	cout << "TIME:::::" << endtime - starttime << endl;
+	//Sleep(1000);
 	return 0;
 }
